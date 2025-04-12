95717f93fb04e0aa72faff7664f327684f158390
LimdaeIl <optional94@naver.com>
2025-04-12 22:31:05 +0900

    final code

---
+++ service/hot-article/src/main/java/com/book/hotArticle/HotArticleApplication.java
@@ -0,0 +1,11 @@
+package com.book.hotArticle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotArticleApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(HotArticleApplication.class, args);
+  }
+}
